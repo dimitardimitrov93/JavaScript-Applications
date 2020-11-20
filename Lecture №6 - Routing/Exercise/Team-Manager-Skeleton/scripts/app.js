b2aed062a50240bdb4ae74ca4947ff331b9aa445
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-11-20 23:47:27 +0200

    commit

--- Lecture №6 - Routing/Exercise/Team-Manager-Skeleton/scripts/app.js
+++ Lecture №6 - Routing/Exercise/Team-Manager-Skeleton/scripts/app.js
@@ -6,6 +6,13 @@ const router = Sammy('#main', function () {
 
     this.use('Handlebars', 'hbs');
 
+    this.get('/', function (context) {
+        loadPartials(context)
+            .then(function () {
+                this.partial('./templates/home/home.hbs');
+            });
+    });
+
     this.get('/home', function (context) {
         loadPartials(context)
             .then(function () {
@@ -68,19 +75,81 @@ const router = Sammy('#main', function () {
 
                 loadPartials(context)
                     .then(function () {
-                        this.partial('./templates/catalog/teamCatalog.hbs', {email, loggedIn, hasNoTeam: hasNoTeam, teams });
+                        this.partial('./templates/catalog/teamCatalog.hbs', { email, loggedIn, hasNoTeam: hasNoTeam, teams });
                     });
             });
     });
 
     this.get('/catalog/:id', function (context) {
         const id = context.params.id;
+        const currentUsername = sessionStorage.email;
 
         loadTeam(id)
-            .then(team => {
+            .then(teamInfo => {
+                const isAuthor = currentUsername === teamInfo.creator ? true : false;
+                let isOnTeam = false;
+                console.log(teamInfo.members);
+                
+                teamInfo.members.forEach(member => {
+                    if (member.username === currentUsername) {
+                        isOnTeam = true;
+                    }
+                });
+
+                const team = {
+                    isAuthor,
+                    isOnTeam,
+                    name: teamInfo.name,
+                    members: teamInfo.members,
+                    comment: teamInfo.comment,
+                    creator: teamInfo.creator,
+                    teamId: id,
+                };
+
+                sessionStorage.setItem('teamId', team.teamId);
+                sessionStorage.setItem('teamName', team.name);
+                sessionStorage.setItem('teamComment', team.comment);
+                sessionStorage.setItem('isAuthor', isAuthor);
+                sessionStorage.setItem('isOnTeam', isOnTeam);
+                sessionStorage.setItem('members', JSON.stringify(team.members));
+                sessionStorage.setItem('creator', team.creator);
+
                 loadPartials(context)
                     .then(function () {
                         this.partial('./templates/catalog/details.hbs', team);
+                    });
+            });
+    });
+
+    this.get('/leave', function (context) {
+        sessionStorage.setItem('hasTeam', false);
+        const userId = sessionStorage.getItem('userId');
+        const hasNoTeam = true;
+        const hasTeam = false
+        const teamId = sessionStorage.getItem('teamId');
+        const teamName = sessionStorage.getItem('teamName');
+        const teamComment = sessionStorage.getItem('teamComment');
+        let members = JSON.parse(sessionStorage.getItem('members'))
+        let memberToRemove;
+
+        members.forEach((member, index) => {
+            if (member.username === sessionStorage.getItem('email')) {
+                memberToRemove = index;
+            }
+        });
+
+        members.splice(memberToRemove, 1);
+        sessionStorage.setItem('members', JSON.stringify(members));
+
+        updateTeam({ name: teamName, comment: teamComment, members, teamId })
+            .then(() => {
+                changeTeamBelonging(userId, hasNoTeam, hasTeam, '')
+                    .then(() => {
+                        loadPartials(context)
+                            .then(function () {
+                                this.partial('./templates/home/home.hbs', { loggedIn: true, email: context.params.email, hasTeam });
+                                displayNotification('You left your team successfully');
+                            });
                     });
             })
     });
@@ -90,17 +159,57 @@ const router = Sammy('#main', function () {
             .then(function () {
                 this.partial('./templates/create/createPage.hbs');
             });
-    })
+    });
+
+    this.get('/edit/:id', function (context) {
+        loadPartials(context)
+            .then(function () {
+
+                const teamInfo = {
+                    name: sessionStorage.getItem('teamName'),
+                    teamId: sessionStorage.getItem('teamId'),
+                    comment: sessionStorage.getItem('teamComment'),
+                }
+
+                this.partial('../templates/edit/editPage.hbs', teamInfo);;
+            });
+    });
+
+    this.post('/edit/:id', function (context) {
+
+        const teamInfo = {
+            name: context.params.name,
+            teamId: context.params.id,
+            comment: context.params.comment,
+            isAuthor: sessionStorage.getItem('isAuthor'),
+            isOnTeam: sessionStorage.getItem('isOnTeam'),
+            members: JSON.parse(sessionStorage.getItem('members')),
+            creator: sessionStorage.getItem('creator'),
+        }
+
+        updateTeam(teamInfo)
+            .then(() => {
+                loadPartials(context)
+                    .then(function () {
+                        displayNotification('Team info updated successfully');
+                        context.redirect(`/catalog/${teamInfo.teamId}`);
+                    });
+            });
+    });
 
     this.post('/create', function (context) {
         const creator = sessionStorage.getItem('email');
         const userId = sessionStorage.getItem('userId');
         const teamName = context.params.name;
         const comment = context.params.comment;
+        const hasNoTeam = false;
+        const hasTeam = true;
+
 
         createTeam(creator, teamName, comment)
-            .then(() => {
-                changeTeamBelonging(userId)
+            .then((res) => {
+                const teamId = res;
+                changeTeamBelonging(userId, hasNoTeam, hasTeam, teamId)
                     .then(() => {
                         this.redirect('/catalog');
                     })
@@ -109,13 +218,6 @@ const router = Sammy('#main', function () {
         loadPartials(context)
             .then(function () {
                 this.partial('./templates/create/createPage.hbs');
-            });
-    })
-
-    this.get('/', function (context) {
-        loadPartials(context)
-            .then(function () {
-                this.partial('./templates/home/home.hbs');
             });
     });
 
@@ -149,21 +251,23 @@ const router = Sammy('#main', function () {
         sessionStorage.setItem('password', password);
         sessionStorage.setItem('loggedIn', true);
 
-        auth.signInWithEmailAndPassword(email, password)
-            .then(function () {
-                loadPartials(context)
-                    .then(function () {
-                        this.partial('./templates/home/home.hbs', { loggedIn: true, email: context.params.email });
-                        displayNotification('Successfully logged in.')
-                    });
-            })
-            .catch(error => displayError(error.message));
-
         getUserInfo(email)
             .then(userInfo => {
                 sessionStorage.setItem('userId', userInfo.id);
                 sessionStorage.setItem('hasNoTeam', userInfo.hasNoTeam);
+                sessionStorage.setItem('hasTeam', userInfo.hasTeam);
+                sessionStorage.setItem('teamId', userInfo.teamId);
             });
+
+        auth.signInWithEmailAndPassword(email, password)
+            .then(function () {
+                loadPartials(context)
+                    .then(function () {
+                        this.partial('./templates/home/home.hbs', { loggedIn: true, email: context.params.email, hasTeam: sessionStorage.getItem('hasTeam'), teamId: sessionStorage.getItem('teamId') });
+                        displayNotification('Successfully logged in.')
+                    });
+            })
+            .catch(error => displayError(error.message));
     });
 
 });
@@ -182,8 +286,10 @@ function loadPartials(context) {
         'catalog': './templates/catalog/teamCatalog.hbs',
         'team': './templates/catalog/team.hbs',
         'details': './templates/catalog/details.hbs',
+        'teamMember': './templates/catalog/teamMember.hbs',
         'teamControls': './templates/catalog/teamControls.hbs',
         'createForm': './templates/create/createForm.hbs',
+        'editForm': './templates/edit/editForm.hbs',
     });
 }
 
@@ -230,7 +336,9 @@ async function getUserInfo(email) {
             userInfo = {
                 id: user[0],
                 email: user[1].email,
-                hasNoTeam: user[1].hasNoTeam
+                hasNoTeam: user[1].hasNoTeam,
+                hasTeam: user[1].hasNoTeam,
+                teamId: user[1].teamId,
             };
         }
     });
@@ -251,11 +359,13 @@ async function addUserToDb(email) {
     return await res;
 }
 
-async function changeTeamBelonging(userId) {
+async function changeTeamBelonging(userId, hasNoTeam, hasTeam, teamId) {
     const req = fetch(`https://team-manager-js-app.firebaseio.com/users/${userId}.json`, {
         method: 'PATCH',
         body: JSON.stringify({
-            hasNoTeam: false,
+            hasNoTeam,
+            hasTeam,
+            teamId,
         }),
     });
 
@@ -268,14 +378,35 @@ async function createTeam(creator, teamName, comment) {
     const req = fetch('https://team-manager-js-app.firebaseio.com/teams.json', {
         method: 'POST',
         body: JSON.stringify({
-            creator, 
-            name: teamName, 
+            creator,
+            name: teamName,
             comment,
-            members: [],
+            members: [{ username: creator }],
         }),
     });
 
     const res = await req;
 
     return await res.json();
+}
+
+async function updateTeam(team) {
+    const req = fetch(`https://team-manager-js-app.firebaseio.com/teams/${team.teamId}.json`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            name: team.name,
+            comment: team.comment,
+            members: team.members,
+        }),
+    });
+
+    const res = await req;
+
+    return await res.json();
+}
+
+async function leaveTeam(teamId, usereMail) {
+    const req = fetch(`https://team-manager-js-app.firebaseio.com/teams/${teamId}.json`, {
+        method: 'PATCH'
+    })
 }
