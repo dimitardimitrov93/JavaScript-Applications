6386f4dae7228cf462f98a9d84cfb75be82abaca
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-11-25 19:38:41 +0200

    commit

---
+++ Workshop Single Page Application/Exercise/JS_App_Retake_Resourses/scripts/init.js
@@ -0,0 +1,187 @@
+function attachEvents() {
+    const navigationTemplate = Handlebars.compile(document.getElementById('navigation-template').innerHTML);
+    const productCardTemplate = Handlebars.compile(document.getElementById('product-card-template').innerHTML);
+    // const editMovieTemplate = Handlebars.compile(document.getElementById('edit-movie-template').innerHTML);
+
+    Handlebars.registerPartial('navigation-template', navigationTemplate);
+    Handlebars.registerPartial('product-card-template', productCardTemplate);
+    // Handlebars.registerPartial('edit-movie-template', editMovieTemplate);
+
+    navigate('/home');
+}
+
+attachEvents();
+
+window.addEventListener('popstate', (e) => {
+    navigate(location.pathname);
+});
+
+function navigationHandler(e) {
+    e.preventDefault();
+
+    if (e.target.tagName === 'A' && e.target.href) {
+        const url = new URL(e.target.href);
+        navigate(url.pathname);
+    } else if (e.target.tagName === 'BUTTON') {
+        const url = new URL(e.target.parentElement.href);
+        navigate(url.pathname);
+    } else if (e.target.id === 'home-link') {
+        navigate('/home');
+    }
+}
+
+function onRegisterSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(document.forms['register-form']);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const repeatedPassword = formData.get('repeatPassword');
+
+    if (!email.trim()) {
+        displayErrorNotification('The email input must be filled.');
+        return;
+    } else if (password !== repeatedPassword) {
+        displayErrorNotification('Password mismatch.');
+        return;
+    } else if (password.length < 6) {
+        displayErrorNotification('The password should be at least 6 characters long.');
+        return;
+    }
+
+    authService.register(email, password)
+        .then(data => {
+            navigate('/home');
+            displaySuccessNotification('Successful registration!');
+        })
+        .catch(error => {
+            let errorMessage = error.message
+                .replace(new RegExp(/_/g), ' ')
+                .split(' ')
+                .map(word => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`)
+                .join(' ');
+            displayErrorNotification(`${errorMessage}.`)
+        });
+}
+
+function onLoginSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(document.forms['login-form']);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    authService.login(email, password)
+        .then(data => {
+            navigate('/home');
+            displaySuccessNotification('Login successful.');
+        })
+        .catch(error => {
+            let errorMessage = error.message
+                .replace(new RegExp(/_/g), ' ')
+                .split(' ')
+                .map(word => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`)
+                .join(' ');
+            displayErrorNotification(`${errorMessage}.`)
+        });
+}
+
+function onCreatedOfferSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(document.forms['create-offer-form']);
+    const name = formData.get('name');
+    const price = formData.get('price');
+    const imageUrl = formData.get('imageUrl');
+    const description = formData.get('description');
+    const brand = formData.get('brand');
+    const creator = JSON.parse(localStorage.getItem('auth')).email;
+
+    if (!name.trim() || !description.trim() || !imageUrl.trim() || !price.trim() || !brand.trim()) {
+        displayErrorNotification('Invalid inputs!');
+        return;
+    }
+
+    productService.add({
+        name,
+        price,
+        description,
+        imageUrl,
+        brand,
+        creator,
+        peopleBought: [""], // check
+    })
+        .then(res => {
+            displaySuccessNotification('Created successfully!');
+            navigate('/home');
+        })
+        .catch(error => displayErrorNotification(error.message));
+}
+
+function onEditProductSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(document.forms['editMovieForm']);
+
+    const productData = {
+        name: formData.get('name'),
+        price: formData.get('price'),
+        imageUrl: formData.get('imageUrl'),
+        description: formData.get('description'),
+        brand: formData.get('brand'),
+    }
+
+    const productId = location.href.replace(new RegExp(/.*\//), '');
+
+    if (!productData.name.trim() || !productData.description.trim() || !productData.imageUrl.trim() || !productData.price.trim() || !productData.brand.trim()) {
+        displayErrorNotification('Invalid inputs!');
+        return;
+    }
+
+    productService.editProduct(productId, productData)
+        .then(res => {
+            navigate(`/details/${productId}`);
+            displaySuccessNotification('Eddited successfully.');
+        })
+        .catch(error => displayErrorNotification(error.message));
+}
+
+async function buyProduct(e, productId) {
+    let currentUser = JSON.parse(localStorage.getItem('auth')).email;
+    let { peopleBought } = await productService.getProduct(productId);
+
+    if (peopleBought.includes('')) {
+        peopleBought.splice(peopleBought.indexOf(''), 1);
+    }
+    peopleBought.push(currentUser);
+
+    await productService.buyProduct(productId, { peopleBought })
+        .then(res => {
+            navigate(`/details/${productId}`);
+        })
+        .catch(error => displayErrorNotification(error.message));
+}
+
+function displaySuccessNotification(message) {
+    const successBoxParagraphElem = document.getElementById('successBox');
+    const successBoxSectionElem = successBoxParagraphElem.parentElement;
+
+    successBoxParagraphElem.innerHTML = message;
+    successBoxSectionElem.style.display = 'block';
+
+    setTimeout(() => {
+        successBoxSectionElem.style.display = 'none';
+    }, 1000);
+}
+
+function displayErrorNotification(message) {
+    const errorBoxParagraphElem = document.getElementById('errorBox');
+    const errorBoxSectionElem = errorBoxParagraphElem.parentElement;
+
+    errorBoxParagraphElem.innerHTML = message;
+    errorBoxSectionElem.style.display = 'block';
+
+    setTimeout(() => {
+        errorBoxSectionElem.style.display = 'none';
+    }, 1000);
+}
+
+function navigate(path) {
+    history.pushState({}, '', path);
+    router(path);
+}
