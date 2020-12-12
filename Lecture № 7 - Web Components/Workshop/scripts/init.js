dfc4f2accc5518981dbc42f9570d6f3ebe3fc4ba
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-12-12 18:24:05 +0200

    Vaadin router implementation.

---
+++ Lecture № 7 - Web Components/Workshop/scripts/init.js
@@ -0,0 +1,11 @@
+import {Router} from 'https://unpkg.com/@vaadin/router';
+
+const root = document.getElementById('root');
+const router = new Router(root);
+
+router.setRoutes([
+  {path: '/', component: 'home-component'},
+  {path: '/register', component: 'register-component'},
+  {path: '/login', component: 'login-component'},
+  {path: '/logout', component: 'logout-component'},
+]);
