cbc0a2b5dda80c376921c9febfb8003e4dbfcaf1
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-12-13 16:19:37 +0200

    Commit

--- Lecture № 7 - Web Components/Workshop/scripts/init.js
+++ Lecture № 7 - Web Components/Workshop/scripts/init.js
@@ -3,10 +3,16 @@ import { logOut } from '../services/authServices.js';
 import Home from '/components/home.js';
 import Register from '/components/register.js';
 import Login from '/components/login.js';
+import Movies from '/components/movies.js';
+import MovieCard from '/components/movie-card.js';
+import MovieDetails from '/components/movie-details.js';
 
 customElements.define('home-component', Home);
 customElements.define('register-component', Register);
 customElements.define('login-component', Login);
+customElements.define('movies-component', Movies);
+customElements.define('movie-card-component', MovieCard);
+customElements.define('movie-details-component', MovieDetails);
 
 const root = document.getElementById('root');
 const router = new Router(root);
@@ -15,6 +21,7 @@ router.setRoutes([
     { path: '/', component: 'home-component' },
     { path: '/register', component: 'register-component' },
     { path: '/login', component: 'login-component' },
+    { path: '/details/:movieId', component: 'movie-details-component' },
     {path: '/logout', action: (context, commands) => {
         logOut();
         return commands.redirect('/');
