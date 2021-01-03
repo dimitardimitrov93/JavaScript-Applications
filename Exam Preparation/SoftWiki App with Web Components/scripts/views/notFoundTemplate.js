16bae75e8501b1b9b250ce18d5a09ee0675d410c
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2021-01-03 14:19:38 +0200

    commit

---
+++ Exam Preparation/SoftWiki App with Web Components/scripts/views/notFoundTemplate.js
@@ -0,0 +1,5 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+
+export default () => html`
+    <h1 style="color: white; text-align: center; padding-top: 2em;">Page not found.</h1>
+`;
